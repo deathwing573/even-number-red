4e0c4deaaeb0b41ebb7c65bb27d5837456818302
mschan73 <shingshing125@gmail.com>
2020-06-06 01:43:47 +0800

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const PORT= process.env.PORT || 5000;
 
-app.use('/', require("./router/main"))
+
 
 if(process.env.NODE_ENV === "production")
 {
@@ -11,6 +11,7 @@ if(process.env.NODE_ENV === "production")
 
     app.get('*',(req,res) =>res.sendFile(path.resolve(__dirname,'date-red-master','build','index.html')))
 }
+app.use('/', require("./router/main"))
 app.listen(PORT, () => {
     console.log('App listening on port 5000!');
 });
